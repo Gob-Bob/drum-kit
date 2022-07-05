d6bd5b9b6847fe57e0c188eed0346c6e8ca43d1e
Tyler <nguyentyler97@gmail.com>
2022-07-04 19:42:26 -0700

    Add yellow glowing on button click

--- javascript.js
+++ javascript.js
@@ -6,12 +6,26 @@ function addSoundToButton(buttonNum, sound) {
 });
 }
 
-soundsArray = ["clap", "hihat", "kick", 
+function addSoundsToAllButtons() {
+    soundsArray = ["clap", "hihat", "kick", 
     "openhat", "boom", "ride", 
     "snare", "tom", "tink"];
 
-let soundCount = 1;
-for (eachSound of soundsArray) {
-    addSoundToButton(`${soundCount}`, `${eachSound}`);
-    soundCount++;
+    let soundCount = 1;
+    for (eachSound of soundsArray) {
+        addSoundToButton(`${soundCount}`, `${eachSound}`);
+        soundCount++;
+    }
 }
+
+addSoundsToAllButtons();
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.style.cssText = 'box-shadow: 0px 0px 4px 4px rgba(255, 255, 0, 0.8);';
+    setTimeout(() => {
+        button.style.cssText = 'box-shadow: '; 
+    }, 50);
+  });
+});
